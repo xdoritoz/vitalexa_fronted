36868dcc67e8477134a3093708aa38ce1a897482
Jose Mendez <josealbertomendez13@gmail.com>
2025-12-27 03:39:14 -0500

    responsive fixed

--- src/pages/VendedorDashboard.js
+++ src/pages/VendedorDashboard.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import client from '../api/client';
 import { useToast } from '../components/ToastContainer';
 import NotificationService from '../services/NotificationService';
